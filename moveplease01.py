adeab39fc04ffd893afc6d410ddcaf9935454ea9
RaylanRod <Raylan.Rodriguez@tlgcohort.com>
2023-07-13 17:32:23 +0000

    renaming and moving files

---
+++ moveplease01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+os.chdir('/home/student/mycode/')
+
+shutil.move('raynor.obj', 'ceph_storage/')
+
+shutil.move('raynor.obj', 'ceph_storage/')
+
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+

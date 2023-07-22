ac79e018ee25934abdbe6aac18ad088c60b6de4d
RaylanRod <Raylan.Rodriguez@tlglearning.com>
2023-07-21 18:48:20 -0700

    all projects completed, ready to submit

--- projects/Project-3/alta3research-flask01.py
+++ projects/Project-3/alta3research-flask01.py
@@ -1,10 +1,34 @@
+"""
+Flask Weather App
+
+This Flask app provides a simple weather information service using the OpenWeatherMap API.
+Users can input a city name, and the app will retrieve and display the current weather data,
+including temperature in Celsius and Fahrenheit, and a weather description.
+
+Endpoints:
+1. /: Main route to input a city name and get weather information.
+2. /weather: Alternate route to get weather information using a POST request.
+3. /weather/<city>: Retrieves weather information for the specified city.
+
+Dependencies:
+- Flask: A micro web framework for Python.
+- requests: Library to make HTTP requests to the OpenWeatherMap API.
+- dotenv: Library to load environment variables from a .env file.
+
+Note:
+This app requires a valid OpenWeatherMap API key stored as 'OPENWEATHERMAP_API_KEY' in a .env file.
+
+Usage:
+Run the app and visit 'http://localhost:2224/' in your web browser to access the weather service.
+"""
+
 from flask import Flask, render_template, jsonify, request
 import requests
 import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-load_dotenv()
+load_dotenv()  # Parse a .env file and then load all the variables found as environment variables.
 
 # Hidden API key
 API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
@@ -13,42 +37,40 @@ BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Check if the request method is POST
     if request.method == 'POST':
-        # If it's a POST request, get the 'city' data from the form
-        city = request.form['city']
-        # Call the get_weather() function with the 'city' data and return the result
-        return get_weather(city)
+        city = request.form['city']  # Get the 'city' data from the submitted form
+        return get_weather(city)  # Call the get_weather() function with the 'city' data and return the result
     else:
-        # If it's not a POST request (i.e., it's a GET request), render the 'get_city.html' template
-        # without trying to access form data, as there won't be any form data in a GET request
-        return render_template('get_city.html')
+        return render_template('get_city.html')  # Render the 'get_city.html' template for the form
 
 
 @app.route('/weather', methods=['POST'])
 def weather():
     if request.method == 'POST':
-        city = request.form['city']
-        return get_weather(city)
-    return render_template('get_city.html')
+        city = request.form['city']  # Get the 'city' data from the submitted form
+        return get_weather(city)  # Call the get_weather() function with the 'city' data and return the result
+    return render_template('get_city.html')  # Render the 'get_city.html' template for the form
 
 
 @app.route('/weather/<city>')
 def get_weather(city):
-    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
-    response = requests.get(BASE_URL, params=params)
+    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}  # Set up parameters for the API request
+    response = requests.get(BASE_URL, params=params)  # Make a GET request to the OpenWeatherMap API
 
-    if response.status_code == 200:
-        data = response.json()
+    if response.status_code == 200:  # If the API request is successful
+        data = response.json()  # Get the JSON data from the response
         weather_info = {
-            'city': data['name'],
-            'temperature': data['main']['temp'],
-            'description': data['weather'][0]['description']
+            'city': data['name'],  # Extract city name from the data
+            'temperature': {
+                'fahrenheit': (data['main']['temp'] * 9 / 5) + 32,  # Convert temperature to Fahrenheit
+                'celsius': data['main']['temp']  # Temperature in Celsius
+            },
+            'description': data['weather'][0]['description']  # Get the weather description
         }
-        return jsonify(weather_info)
+        return jsonify(weather_info)  # Return the weather information as JSON response
 
-    return jsonify({'error': 'City not found'}), 404
+    return jsonify({'error': 'City not found'}), 404  # Return error response if city is not found
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2224, debug=True)
+    app.run(host='0.0.0.0', port=2224, debug=True)  # Start the Flask server on host and port, with debugging enabled
--- projects/Project-3/alta3research-requests02.py
+++ projects/Project-3/alta3research-requests02.py
@@ -3,9 +3,9 @@ import requests
 from pprint import pprint
 
 URL = "http://127.0.0.1:2224/weather"  # Use the '/weather' endpoint for the POST request
-city = "Seattle"  # Replace this with the city you want to get the weather for
+city = input("Which city in the United States would you like to search: ")
 
 # Make a POST request with the 'city' data in the payload
 resp = requests.post(URL, data={'city': city}).json()
 
-pprint(resp)
+pprint(resp)  # pretty print --> to print in a way the user will understand
